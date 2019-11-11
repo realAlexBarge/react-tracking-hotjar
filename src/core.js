78d590c7a4c5eb6a45c4af466805eb50f494e81a
Alexander Barge <alexander.barge@gmail.com>
2019-11-11 10:13:52 +0100

    add try catch

--- src/core.js
+++ src/core.js
@@ -10,24 +10,29 @@ export function getTracker() {
 }
 
 export function initialize(newTrackerId) {
-  /* eslint-disable */
-  (function(h, o, t, j, a, r) {
-    h.hj =
-      h.hj ||
-      function() {
-        (h.hj.q = h.hj.q || []).push(arguments);
+  try {
+    /* eslint-disable */
+    (function(h, o, t, j, a, r) {
+      h.hj =
+        h.hj ||
+        function() {
+          (h.hj.q = h.hj.q || []).push(arguments);
+        };
+      h._hjSettings = {
+        hjid: newTrackerId,
+        hjsv: 6,
       };
-    h._hjSettings = {
-      hjid: newTrackerId,
-      hjsv: 6,
-    };
-    a = o.getElementsByTagName('head')[0];
-    r = o.createElement('script');
-    r.async = 1;
-    r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
-    a.appendChild(r);
-  })(window, document, 'https://static.hotjar.com/c/hotjar-', '.js?sv=');
-  /* eslint-enable */
+      a = o.getElementsByTagName('head')[0];
+      r = o.createElement('script');
+      r.async = 1;
+      r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
+      a.appendChild(r);
+    })(window, document, 'https://static.hotjar.com/c/hotjar-', '.js?sv=');
+    /* eslint-enable */
+  } catch (e) {
+    // eslint-disable-next-line no-console
+    console.warn(e);
+  }
 }
 
 export default {
